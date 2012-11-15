6b5d1e2ddddd49b5fd1088fa82f94c7ad04c9a4c
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2012-11-14 22:13:43 -0800

    added testing code to test php file

--- Java/danger_zone/test.php
+++ Java/danger_zone/test.php
@@ -2,13 +2,15 @@
 <?php
 //Danger Socket Test
 
-$message = "LON 91.12 LAT 40.78 NUM 3\r";
+
 $domain = (strtoupper(substr(PHP_OS, 0, 3)) == 'WIN' ? AF_INET : AF_INET);
 $socketHandle = socket_create($domain, SOCK_DGRAM, SOL_UDP);
 $serverIP = "127.0.0.1";
 $serverSendPort = 5480;
 $serverRecvPort = 5480;
 
+//GEO CORDINATE TEST
+$message = "LON 91.12 LAT 40.78 NUM 3\r";
 socket_bind($socketHandle, $serverIP, $serverSendPort);
 
 echo 'MESSAGE: ' . $message . '<br />';
@@ -21,6 +23,7 @@ socket_recvfrom($socketHandle, $response, 512, 0, $from, $port);
 echo $response . '<br />';
 
 
+//CLASSIFY TEST
 $message = "CLASSIFY Gunshots in Syria";
 echo 'MESSAGE: ' . $message . '<br />';
 socket_sendto($socketHandle, $message, strlen($message), MSG_EOF, $serverIP, $serverSendPort);
@@ -31,6 +34,18 @@ $port = 0;
 socket_recvfrom($socketHandle, $response, 512, 0, $from, $port);
 echo $response . '<br />';
 
+//TRAINING TEST
+$message = "TRAIN D Bombs over london";
+echo 'MESSAGE: ' . $message . '<br />';
+socket_sendto($socketHandle, $message, strlen($message), MSG_EOF, $serverIP, $serverSendPort);
+
+$response = "";
+$from = "";
+$port = 0;
+socket_recvfrom($socketHandle, $response, 512, 0, $from, $port);
+echo $response . '<br />';
+
+//KILL TEST (see server for output)
 $message = 'KILLSERVER0x0000';
 echo 'MESSAGE: ' . $message . '<br />';
 socket_sendto($socketHandle, $message, strlen($message), MSG_EOF, $serverIP, $serverSendPort);
